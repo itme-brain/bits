f938ae820929d11a51b38669862b878eaa192ee9
Bryan Ramos <bryan@ramos.codes>
2023-09-06 00:07:11 -0400

    CC0

---
+++ components/CC0.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p xmlns:dct="http://purl.org/dc/terms/" xmlns:vcard="http://www.w3.org/2001/vcard-rdf/3.0#">
+      <a rel="license" href="http://creativecommons.org/publicdomain/zero/1.0/">
+        <img src="http://i.creativecommons.org/p/zero/1.0/88x31.png" style="border-style: none;" alt="CC0" />
+      </a>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+img {
+  @apply flex justify-center items-center select-none
+}
+</style>
