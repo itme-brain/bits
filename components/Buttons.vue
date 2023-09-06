57ebeeff8a068e39172670922a9f12946d458f8d
Bryan Ramos <bryan@ramos.codes>
2023-09-06 00:03:44 -0400

    kit buttons

---
+++ components/Buttons.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="buttons">
+    <a href="BitsSymbolGraphicKit.zip" class="button-left">
+      Graphics Kit
+    </a>
+    <a href="/font" class="button-right">
+      Font Kit
+    </a>
+  </div>
+</template>
+
+
+<script lang="ts"></script>
+
+<style scoped>
+.buttons {
+  @apply flex max-w-lg mx-auto rounded-lg mt-10 select-none
+}
+
+.button-left, .button-right {
+  @apply text-white text-lg font-bold font-sans bg-purple-600
+}
+
+.button-left {
+  @apply flex-1 text-center p-4 border-r border-white rounded-l-lg
+}
+
+.button-right {
+  @apply flex-1 text-center p-4 rounded-r-lg
+}
+
+.button-left:hover, .button-right:hover {
+  @apply bg-orange-500
+}
+</style>
