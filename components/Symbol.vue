ecb12b7f0c0848376b5ce0d683d32dafc191004e
Bryan Ramos <bryan@ramos.codes>
2023-09-06 00:01:37 -0400

    bits symbol

---
+++ components/Symbol.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img src="/assets/BitsSymbolCoin.svg" alt="Bits Symbol" class="image">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+
+
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+.image {
+  @apply h-96 w-96
+}
+</style>
