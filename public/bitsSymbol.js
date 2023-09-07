c7aba6ef16a0002700089a28d9d8f749431e9d0c
Bryan Ramos <bryan@ramos.codes>
2023-09-07 10:58:59 -0400

    yet another script fix

--- public/bitsSymbol.js
+++ public/bitsSymbol.js
@@ -15,10 +15,10 @@
     const spanMap = {
       'i.Bits-Symbol': `<span class="icon-Bits-Symbol"></span>`,
       'i.Bits-Symbol_noLine': `<span class="icon-Bits-Symbol_noLine"></span>`,
-      'i.Bits-Symbol_tilt': `<span class="icon-Bits-Symbol_slanted"></span>`,
-      'i.Bits-Symbol_tilt_noLine': `<span class="icon-Bits-Symbol_slanted_noLine"></span>`,
+      'i.Bits-Symbol_tilt': `<span class="icon-Bits-Symbol_tilt"></span>`,
+      'i.Bits-Symbol_tilt_noLine': `<span class="icon-Bits-Symbol_tilt_noLine"></span>`,
 
-      'i.Bits-Symbol-Coin': `<span class="icon-Bits-Symbol-coin"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>`,
+      'i.Bits-Symbol-Coin': `<span class="icon-Bits-Symbol-Coin"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>`,
       'i.Bits-Symbol-Coin_tilt': `<span class="icon-Bits-Symbol-Coin_tilt"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>`,
       'i.Bits-Symbol-Coin_noLine': `<span class="icon-Bits-Symbol-Coin_noLine"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>`,
       'i.Bits-Symbol-Coin_tilt_noLine': `<span class="icon-Bits-Symbol-Coin_tilt_noLine"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>`,
